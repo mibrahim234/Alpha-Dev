cecc05708d4225af1be5641d70ac9c97c793d8f3
mibrahim234 <mohamm1417@gmail.com>
2022-01-11 21:11:26 -0500

    build file/app.css

--- server/server.js
+++ server/server.js
@@ -24,15 +24,15 @@ app.use(express.json());
 
 
 // production 
-//  if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../client/build')));
-//   }
+ if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
   
   
  // production
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-//   });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
 
 db.once('open', () => {
