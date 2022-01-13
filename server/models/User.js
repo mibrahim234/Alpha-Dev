e1b3ce8d7683da72873e2b723ce14bff7f4ad32d
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2022-01-12 22:28:04 -0500

    added User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,70 +1,54 @@
-// const { Schema, model } = require('mongoose');
-// // password hashing 
-// const bcrypt = require('bcrypt');
+const { Schema, model } = require('mongoose');
+// password hashing 
+const bcrypt = require('bcrypt');
 
-// const userSchema = new Schema(
-//   {
-//     username: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       trim: true
-//     },
-//     email: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       // validation with regex: follows the pattern <string>@<string>.<string>.if error send back msg
-//       match: [/.+@.+\..+/, 'Must match an email address!']
-//     },
-//     password: {
-//       type: String,
-//       required: true,
-//       minlength: 5
-//     },
-//     thoughts: [
-//       {
-//         type: Schema.Types.ObjectId,
-//         ref: 'Thought'
-//       }
-//     ],
-//     friends: [
-//       {
-//         type: Schema.Types.ObjectId,
-//         ref: 'User'
-//       }
-//     ]
-//   },
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // validation with regex: follows the pattern <string>@<string>.<string>.if error send back msg
+      match: [/.+@.+\..+/, 'Must match an email address!']
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5
+    }
+  },
 //   {
 //     toJSON: {
 //       virtuals: true
 //     }
 //   }
-// );
+);
 
-// // set up pre-save middleware to create password
-// // bcrypt hashing 
-// // check to see if the data is new or if the password has been modified
-// // we can use this same middleware for both new users and users who are updating their information but don't want to update their password values.
-// userSchema.pre('save', async function(next) {
-//   if (this.isNew || this.isModified('password')) {
-//     const saltRounds = 10;
-//     this.password = await bcrypt.hash(this.password, saltRounds);
-//   }
+// set up pre-save middleware to create password
+// bcrypt hashing 
+// check to see if the data is new or if the password has been modified
+// we can use this same middleware for both new users and users who are updating their information but don't want to update their password values.
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
 
-//   next();
-// });
+  next();
+});
 
-// // compare the incoming password with the hashed password
-// userSchema.methods.isCorrectPassword = async function(password) {
-//   return bcrypt.compare(password, this.password);
-// };
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return bcrypt.compare(password, this.password);
+};
 
-// userSchema.virtual('friendCount').get(function() {
-//   return this.friends.length;
-// });
+const User = model('User', userSchema);
 
-// const User = model('User', userSchema);
-
-// module.exports = User;
+module.exports = User;
 
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+const { User } = require("../models");
+
+const resolvers = {
+    Query: {
+
+    }
+  };
+  
+  module.exports = resolvers;
