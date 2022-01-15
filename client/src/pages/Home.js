528ee13d51da34689d5666c7ff64083fd67e07a5
mibrahim234 <mohamm1417@gmail.com>
2022-01-15 02:25:16 -0500

    homepage done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,20 +9,21 @@ const Home = () => {
     return (
         
         <div>
-            
-            <h1 className='center'>We connect companies eager to succeed with talented developers! </h1>
 
-          <img src="/images/developer.jpg" alt="Developer Working on code"/>
+
+            <h1 className='page-title m-2 py-5'>Welcome to AlphaDev! <br></br>We connect companies eager to succeed with talented developers. </h1>
+
+          <img src="/images/developer.jpg" alt="Developer Working on code" className='mb-4'/>
 
           <div className="container flex-row justify-space-between justify-center align-center">
 
-          <button className="btn1 m-4"> <Link to="/developers">Companies - Find Talent</Link></button>
+          <button className="btn1 m-4 py-3"> <Link to="/developers">Companies - Find Talent</Link></button>
 
-          <button className="btn1 m-4"><Link to="/companies">Developers - Find An Opportunity</Link></button>
+          <button className="btn1 m-3 py-3"><Link to="/companies">Developers - Find An Opportunity</Link></button>
 
           </div>
 
-            <h1 className='center'>Matching talent with Opportunity.</h1>
+            <h1 className='page-title m-4'>Matching talent with Opportunity.</h1>
 
             <img src="/images/teamwork.jpg" alt="People working together" className='img1'/>
 
