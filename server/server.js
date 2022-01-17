e458648804ced6d91ed5b8e3b6e9322412f95fed
mibrahim234 <mohamm1417@gmail.com>
2022-01-17 03:03:27 -0500

    heroku deployment failed

--- server/server.js
+++ server/server.js
@@ -32,9 +32,9 @@ app.use(express.json());
   
   
 // //  // production
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-//   });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // });
 
 
 db.once('open', () => {
