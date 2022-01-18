a7ec2d872ac2d09a381171b380e51efa349ebf16
mibrahim234 <mohamm1417@gmail.com>
2022-01-18 17:41:10 -0500

    developer seed data?

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -19,7 +19,51 @@ db.once('open', async () => {
 
   await User.collection.insertMany(userData);
 
-
+  // Developer Seed Data 
+// {
+//   "Developer": [
+//     {
+//       "preferredRole": "Front End",
+//       "language": "HTML+CSS",
+//       "availability": "One month",
+//       "commitment": "Full-Time",
+//       "startDate": "Immediately",
+//       "id": 1
+//     },
+//     {
+//       "preferredRole": "Back End",
+//       "language": "JavaScript",
+//       "availability": "Two Weeks Or Less",
+//       "commitment": "Part-Time",
+//       "startDate": "One To Two Weeks",
+//       "id": 2
+//     },
+//     {
+//       "preferredRole": "Full Stack",
+//       "language": "React",
+//       "availability": "Three Months",
+//       "commitment": "Hourly",
+//       "startDate": "One Month",
+//       "id": 3
+//     },
+//     {
+//       "preferredRole": "Back End",
+//       "language": "Python",
+//       "availability": "Six Months",
+//       "commitment": "Full-Time",
+//       "startDate": "I'll Decide Later",
+//       "id": 4
+//     },
+//     {
+//       "preferredRole": "Front End",
+//       "language": "React",
+//       "availability": "One month",
+//       "commitment": "Full-Time",
+//       "startDate": "Immediately",
+//       "id": 5
+//     }
+//   ]
+// }
 //   for (let i = 0; i < 5; i++) {
 //     const title = faker.company.catchPhrase();
 //     const role = faker.name.findName();
