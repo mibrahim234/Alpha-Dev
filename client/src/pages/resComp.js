25e2cc15d05f128e4cc388343348a0f3a84bd1ba
mibrahim234 <mohamm1417@gmail.com>
2022-01-18 16:38:12 -0500

    fixed up responsecomp page

--- client/src/pages/resComp.js
+++ client/src/pages/resComp.js
@@ -8,12 +8,17 @@ import Typography from '@mui/material/Typography';
 
 const resComp = () => {
     return (
-        <div className="container flex-row justify-space-between justify-center align-center">
+        <div className="container1 flex-row justify-space-between justify-center align-center">
        
         <Card>
         <CardContent className="card1">
         <h1 className="mb-2 py-2 title text-center">Here are your results!</h1>
-        <h2 className="mb-2 py-2 text-center"> We will contact you shortly!</h2>
+        {/* <h2 className="mb-2 py-2 text-center"> We will contact you shortly!</h2> */}
+        <CardContent className="card1">
+        <Typography>
+         <h1 className="text-center"> Lateefah Jana Vicario </h1>
+        </Typography>
+      </CardContent>
         </CardContent>
       <CardMedia
         component="img"
@@ -25,10 +30,7 @@ const resComp = () => {
       
       <CardContent className="card1">
         <Typography>
-         <h1> Lateefah Jana Vicario </h1>
-        </Typography>
-        <Typography>
-        <h3>Hi, I'm a full stack web developer with senior level experience. <br></br>Interested in a cross-functional software development role, that touches all parts of the stack. Looking for a team of humble and talented people which embrace new technology and encourage autonomy. </h3>
+        <h3>Hi, I'm a full stack web developer with senior level experience. <br></br>Interested in a cross-functional software development role, that touches all parts of the stack. <br></br> Looking for a team of humble and talented people which embrace new technology and encourage autonomy. </h3>
         </Typography>
       </CardContent>
 
