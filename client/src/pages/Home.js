8f884f40df01aa795cf1b2c815aafe417f348fd4
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2022-01-19 18:27:24 -0500

    updated navigation via authorization

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { useQuery } from '@apollo/client';f
 import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
 
 // make pages for company and developers 
 // need to add responsiveness 
@@ -13,22 +14,28 @@ const Home = () => {
 
             <h1 className='page-title m-2 py-5'>Welcome to AlphaDev! <br></br>We connect companies eager to succeed with talented developers. </h1>
 
-          <img src="/images/developer.jpg" alt="Developer Working on code" className='mb-4'/>
+            <img src="/images/developer.jpg" alt="Developer Working on code" className='mb-4'/>
 
-          <div className="container flex-row justify-space-between justify-center align-center">
+            <div className="container flex-row justify-space-between justify-center align-center">
+              {Auth.loggedIn() ? (
+                <>
+                  {/* <button className="btn1 m-4 py-3"> <Link to="/companies">Companies - Find Talent</Link></button> */}
 
-          {/* <button className="btn1 m-4 py-3"> <Link to="/companies">Companies - Find Talent</Link></button> */}
+                  <button className="btn1 m-3 py-3"><Link to="/developers">Developers - Find An Opportunity</Link></button>
+                </>
+              ) : (
+                <></>
+              )}
 
-          <button className="btn1 m-3 py-3"><Link to="/developers">Developers - Find An Opportunity</Link></button>
-
-          </div>
+              
+            </div>
 
             <h1 className='page-title m-4'>Matching talent with Opportunity.</h1>
 
             <img src="/images/shakinghand.jpg" alt="Boss Shaking Hand, representing opportunity" className='mb-4'/>
 
            
-          </div>
+        </div>
 
     )
   };
