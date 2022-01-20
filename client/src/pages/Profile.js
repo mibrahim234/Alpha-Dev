bbf698c7f3e6552a50e032e26b18275b6ef8bea2
mibrahim234 <mohamm1417@gmail.com>
2022-01-20 02:03:02 -0500

    working on profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -13,11 +13,18 @@ const Profile = () => {
         setProfile(data)
     }, [data])
 
-    return (
-        <div>
 
+    //     Have a card component here with username, email
+    // --developers choices and delete icon on the right of the card, change info button?, takes you back to the developer page
+
+    return (
+        <div className="container flex-row justify-space-between justify-center align-center">
+            
             <h1 className="mb-2 py-2 title text-center">Here is your current information.</h1>
-                <div>
+           
+
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+
                     <h1>Username: {data.me.username}</h1>
                     <h1>Email: {data.me.email}</h1>
                     <h1>Preferred Role: {data.me.position.preferredRole}</h1>
@@ -25,12 +32,7 @@ const Profile = () => {
                     <h1>Level of Commitment: {data.me.position.length}</h1>
                     <h1>Availability: {data.me.position.commitment}</h1>
                     <h1>When Can You Start: {data.me.position.startDate}</h1>
-                </div>
-            <h2 className="mb-2 py-2 text-center"> Companies will contact you shortly. </h2>
-
-            {/* Have a card component here with username, email
-            --developers choices and delete icon on the right of the card, change info button?, takes you back to the developer page
-             */}
+         </Grid>
         </div>
     )
 };
